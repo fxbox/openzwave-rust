fdd7900277a2af96c1b29444239bbc81796318a1
Julien Wajsberg <felash@gmail.com>
2016-02-16 22:24:53 +0100

    have an environment variable to configure the openzwave include path

---
+++ README.md
@@ -0,0 +1,12 @@
+Open-Zwave wrapper for Rust
+===========================
+
+Installation
+------------
+
+Simply using the crate should work. However if openzwave's includes are not
+localized in `/usr/include/openzwave` or `/usr/local/include/openzwave` you'll
+need to use the environment variable `OPENZWAVE_INCLUDE_PATH` to configure it.
+This is because of [an issue in OpenZWave](https://github.com/OpenZWave/open-zwave/issues/771)
+they don't want to fix.
+
--- openzwave-sys/build.rs
+++ openzwave-sys/build.rs
@@ -1,14 +1,21 @@
 extern crate gcc;
+use std::env;
 
 fn main() {
     let mut c = gcc::Config::new();
-    c.file("openzwave-c/options.cc")
+    let config = c.file("openzwave-c/options.cc")
      .file("openzwave-c/manager.cc")
      .file("openzwave-c/notification.cc")
      .file("openzwave-c/value_classes/value_id.cc")
      .include("/usr/include/openzwave") // workaround for https://github.com/OpenZWave/open-zwave/issues/771
-     .cpp(true)
-     .compile("libopenzwave-c.a");
+     .include("/usr/local/include/openzwave")
+     .cpp(true);
+
+    if let Ok(path) = env::var("OPENZWAVE_INCLUDE_PATH") {
+        config.include(path);
+    }
+
+    config.compile("libopenzwave-c.a");
     println!("cargo:rustc-flags=-l openzwave");
     //println!("cargo:rustc-link-lib=static=foo")
 }
