047da3970a01d62771b4b92d56df5fa1c6c3e904
Julien Wajsberg <felash@gmail.com>
2016-04-08 14:55:26 +0200

    Use the installation path as default config path

--- openzwave-sys/src/lib.rs
+++ openzwave-sys/src/lib.rs
@@ -6,6 +6,16 @@ pub mod options;
 pub mod notification;
 pub mod value_classes;
 
+use std::path::PathBuf;
+
+pub fn get_default_config_path() -> PathBuf {
+    let installed_path = env!("CARGO_MANIFEST_DIR");
+    let mut installed_path = PathBuf::from(installed_path);
+    installed_path.push("open-zwave");
+    installed_path.push("config");
+    installed_path
+}
+
 #[cfg(test)]
 mod test {
     #[test]
